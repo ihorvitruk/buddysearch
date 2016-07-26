7dd19d70b90d89dad9744dd8476d7c0f66d0dd41
I.Vitruk <ihorvitruk@gmail.com>
2016-07-26 23:42:38 +0300

    init commit

---
+++ app/src/main/java/com/buddysearch/android/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.buddysearch.android;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/com/buddysearch/android/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.buddysearch.android;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
