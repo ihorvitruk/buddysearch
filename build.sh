afc4cb32c179ffa0fcac76b4f79145fc02ddda18
Ihor Vitruk <ihorvitruk@gmail.com>
2016-08-31 19:36:59 +0300

    moody mock-google-services.json

--- build.sh
+++ build.sh
@@ -4,13 +4,8 @@
 set -e
 
 if [ ! -f ./data/google-services.json ]; then
-    echo "Using mock google-services.json for data"
+    echo "Using mock google-services.json"
     cp ./mock-google-services.json ./data/google-services.json
-  fi
-
-  if [ ! -f ./presentation/google-services.json ]; then
-    echo "Using mock google-services.json for presentation"
-    cp ./mock-google-services.json ./presentation/google-services.json
   fi
 
 ./gradlew build
